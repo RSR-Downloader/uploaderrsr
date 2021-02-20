f70d3f645ccf85ea7edc01c3e5bcb13655770026
RSR-Downloader <79363075+RSR-Downloader@users.noreply.github.com>
2021-02-20 21:26:12 +0530

    Update start.py

--- start.py
+++ start.py
@@ -5,9 +5,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("SUPPORT CHANNEL", url="https://t.me/MizoInFoTel14")],
+        [InlineKeyboardButton("SUPPORT CHANNEL", url="https://t.me/mizolibrary")],
         [InlineKeyboardButton(
-            "SUPPORT GROUP", url="https://t.me/MizoInFoTel1")]
+            "SUPPORT GROUP", url="https://t.me/rsrmusicofficial")]
     ])
     welcomed = f"Hi! <b>{message.from_user.first_name}</b>Min hmandan tur i hriat duh chuan /help tih hi click rawh."
     await message.reply_text(welcomed, reply_markup=joinButton)
